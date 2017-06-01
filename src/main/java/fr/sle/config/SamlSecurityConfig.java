54d35ccfd8bc754a604eb6abe1e70417561cea48
slem1 <sylvain.lem1@gmail.com>
2017-06-01 16:04:05 +0200

    Add saml entrypoint

--- src/main/java/fr/sle/config/SamlSecurityConfig.java
+++ src/main/java/fr/sle/config/SamlSecurityConfig.java
@@ -1,8 +1,16 @@
 package fr.sle.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.saml.SAMLEntryPoint;
+import org.springframework.security.saml.websso.WebSSOProfileOptions;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author slemoine
@@ -10,8 +18,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SamlSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public WebSSOProfileOptions defaultWebSSOProfileOptions() {
+        WebSSOProfileOptions webSSOProfileOptions = new WebSSOProfileOptions();
+        webSSOProfileOptions.setIncludeScoping(false);
+        return webSSOProfileOptions;
+    }
+
+    @Bean
+    public SAMLEntryPoint samlEntryPoint() {
+        SAMLEntryPoint samlEntryPoint = new SAMLEntryPoint();
+        samlEntryPoint.setDefaultProfileOptions(defaultWebSSOProfileOptions());
+        return samlEntryPoint;
+    }
+
+    @Bean
+    public FilterChainProxy samlFilter() throws Exception {
+        List<SecurityFilterChain> chains = new ArrayList<>();
+        return new FilterChainProxy(chains);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(samlEntryPoint());
     }
+
 }
