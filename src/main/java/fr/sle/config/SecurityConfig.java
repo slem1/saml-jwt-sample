3d6a6df068e3f7a2e70dc05e9dcd53922a3a644d
slem1 <sylvain.lem1@gmail.com>
2017-06-02 17:11:10 +0200

    Add Jwt support

--- src/main/java/fr/sle/config/SecurityConfig.java
+++ src/main/java/fr/sle/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package fr.sle.config;
 
+import fr.sle.config.jwt.JwtAuthenticationFilter;
+import fr.sle.config.jwt.JwtAuthenticationProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author slemoine
@@ -20,9 +24,22 @@ public class SecurityConfig {
     @Configuration
     @Order(1)
     public static class RestApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        private static final String apiMatcher = "/api/**";
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/api/**").authorizeRequests().anyRequest().authenticated();
+
+            http.addFilterBefore(new JwtAuthenticationFilter(apiMatcher, super.authenticationManager()), UsernamePasswordAuthenticationFilter.class);
+
+            http.authorizeRequests()
+                    .antMatchers(apiMatcher)
+                    .authenticated();
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(new JwtAuthenticationProvider());
         }
     }
 
